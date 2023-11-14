363815aff6906e1794349b2660f13d8a39df72a3
hiany <hianyg81@gmail.com>
2023-11-13 23:16:56 -0300

    Aulas de JavaScript

---
+++ JavaScript/js/index.js
@@ -0,0 +1,14 @@
+//----Aula01 e Aula02----
+
+//Saidas JavaSript
+//São Elas: innerHTML,document.write,alert e console.log
+// innerHTML - Usamos para ter interação com o site
+// document.write -  Usamos  para fazer algun teste
+// alert - usamos alerta para obtermos resultados
+// console.log - usamos document.write para obtermos resultados
+
+
+
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";
+alert("Oi, isso é um alerta");
+console.log("Oi, isso é um console.log");
